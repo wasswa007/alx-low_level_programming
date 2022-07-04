7ac13c222d911bb2fbe95c7466ae7430fa9e3d3f
wasswa007 <lnc19465@gmail.com>
2022-07-04 06:57:50 -0700

    joker

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
-#include <stdio>
+#include <stdio.h>
+#include <string.h>
 
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
 }
